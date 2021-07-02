392d66bf67e374d97c88fead427590923d905d04
Shaheb Ali <Shaheb.Ali@yrbrands.com>
2021-07-02 11:44:22 +0600

    node app for file organizing of your system

---
+++ index.js
@@ -0,0 +1,7 @@
+const {organize, help} = require('./src')
+ 
+
+if (process.argv[2] === 'organize') {
+     organize(process.argv)
+}
+ 
---
+++ src/help.js
@@ -0,0 +1,5 @@
+const help = () => {
+  console.log('Hello World Help');
+};
+
+module.exports = help;
